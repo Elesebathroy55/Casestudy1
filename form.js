91150ca2d5e16b4b6eeeaa981846bfb510d7cbbf
Elesebathroy55 <81869059+Elesebathroy55@users.noreply.github.com>
2021-05-02 23:03:44 +0530

    Update form.js

--- form.js
+++ form.js
@@ -1,79 +1,37 @@
-var x = document.getElementById("email");
-// var y = document.getElementById("pwd");
-//let error= document.getElementById("error");
-
-//function validate(){
-    // if(email.value.trim()=="" || pwd.value.trim() ==""){
-    //     alert("Fields cannot be empty");
-    //     return false;
-    // }
-    // else{
-    //     alert("Validation is proper");
-    //     return true;
-    // }
-    // if(email.value==""){
-    //     alert("Email can't be empty!");
-    //     return false;
-    // }
-    // else if(pwd.value==""){
-    //     alert("Password can't be blank!");
-    //     return false;
-    // }
-    // else if(pwd.value.length<=7){
-    //     alert("Password is too short");
-    //     pwd.style.border="2px solid red";
-    //     return false;
-    // }
-    // else{
-    //     return true;
-    // }
-
-  /*  function validate(callback){
- //let regexp= /^([A-Za-z0-9\.-]+)@([A-Za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3})?$/;
-    if ((username.value == "admin")&& (pwd.value == "12345")){
-        error.innerHTML="Valid";
-        error.style.color="green";
-        return true;
-        callback();
-    }
-    else{
-        alert("Invalid username or Password.!!");
-        error.style.color="red";
-        return false;
-    }}*/
-    var x = username.value;
+  var x = username.value;
     var y = pwd.value;
-    function validate(x, y,callback){
-        if ((username.value == "admin")&& (pwd.value == "12345")){
+    let regexp= /\d*[1-5]{5}$/;
+    
+    function validate(callback){
+        //let regexp= /\d*[1-5]{5}$/;
+        if ((username.value == "admin")&& (pwd.value=="12345")){
             error.innerHTML="Valid";
             error.style.color="green";
             return true;
             var login = true;
-           
+           callback(login);
         }
         else{
             alert("Invalid username or Password.!!");
-            error.style.color="red";
+            error2.innerHTML="Invalid"
+            error2.style.color="red";
             return false;
             var login = false;
+            callback(login);
         }
-        if(login==true){
-        function ajax(){
-        //Creating an XHR object
-       
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function(){
-            if(this.readyState==4&&this.status==200){
-                var response= JSON.parse(this.responseText);
-                var output ="";
-                for(var i =0;i<response.length;i++){
-                    output += "<li>" + response[i].title +"</li>";
-                }
-                document.getElementById("demo").innerHTML=output;
-            }
-        }
-        xhttp.open("GET","https://jsonplaceholder.typicode.com/todos",true);
-        xhttp.send();
-        }}
     }
-       
+        function pass(login){
+
+            if(login=="true"){
+                $(document).ready(function() {
+         
+                    var url = "https\\todo.html";
+                    //$(location).attr('href',url);
+                    window.location.replace("todo.html");
+                   
+                   });
+        }
+        else{
+            console.log("Unsuccesful login");
+        }
+    }
